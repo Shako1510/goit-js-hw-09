77a2f1e936e6d2876dd5b848668b15f0b966e7ae
Shako Yevhen <schako15101992@gmail.com>
2022-08-04 21:52:11 +0300

    Старт завд 3 модуль 9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,15 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+const delayEll = document.querySelector('input[name="delay"]');
+console.log(delayEll);
+const stepEll = document.querySelector('input[name="step"]');
+console.log(stepEll);
+const amountEll = document.querySelector('input[name="amount"]');
+console.log(amountEll);
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
